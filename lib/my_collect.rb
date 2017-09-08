8a9efcbe463418d72e04aa9c18a5c45283135cca
Colin McKean <ezikill@hotmail.com>
2017-09-08 17:10:32 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
 newCollection = []
   while i < collection.size
-    
+
     newCollection.push(collection[i].upcase)
     yield(collection[i].upcase)
     i += 1
